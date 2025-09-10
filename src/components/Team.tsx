50841d5b0546fc4a6ca2e3c66ad94ffe091c7801
Arda9995 <ardaonuk9995@gmail.com>
2025-09-10 09:45:27 +0300

    Update Team.tsx

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -406,7 +406,7 @@ const Team = () => {
           <div className="flex flex-col gap-3">
             {categories.map(([category, members]) => (
                 <div key={category}>
-                  <h3 className="text-2xl font-semibold text-white mb-6 text-center">
+                  <h3 className="text-2xl font-semibold text-white mb-3 text-center">
                     {category}
                   </h3>
                   <div className="flex flex-wrap justify-center gap-6">
